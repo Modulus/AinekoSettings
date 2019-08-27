2ca79a6256fff37a619b7578ba148e428bdc4336
John Sigvald Skauge <johnsigvald.skauge@tv2.no>
2019-08-27 14:22:53 +0200

    Updated

--- src/main/java/com/aineko/settings/SettingsApplication.java
+++ src/main/java/com/aineko/settings/SettingsApplication.java
@@ -1,5 +1,7 @@
 package com.aineko.settings;
 
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +11,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableAutoConfiguration()
 public class SettingsApplication {
 
+
+
 	public static void main(String[] args) {
+		System.out.println(System.getenv());
 		SpringApplication.run(SettingsApplication.class, args);
 
 
